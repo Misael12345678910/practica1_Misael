4b0736049d200f7305b1d84ac3e6f828a3647deb
Misael12345678910 <89798094+Misael12345678910@users.noreply.github.com>
2021-09-06 11:55:40 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+funtion getIMC(){
+  var peso=parseFloat(document.getElementById("peso").value;
+                      var Altura=ParseFloat(document.getElementByld("Altura).value;
+                      var imc=peso/(altura*altura)
+  document.get.ElementById("IMC").innerHTML=IMC
+}
