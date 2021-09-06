e9487438eff664b60d69c678a391ccffa16aafc7
Misael12345678910 <89798094+Misael12345678910@users.noreply.github.com>
2021-09-06 13:08:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-funtion getIMC(){
-  var peso=parseFloat(document.getElementById("peso").value;
-                      var Altura=ParseFloat(document.getElementByld("Altura).value;
-                      var imc=peso/(altura*altura)
-  document.get.ElementById("IMC").innerHTML=IMC
+function getIMC(){
+  var peso=parseFloat(document.getElementById('peso').value);
+  var altura=parseFloat(document.getElementById('altura').value);
+  var imc=peso/(altura*altura);
+  document.getElementById("imc").innerHTML=imc;
 }
+
